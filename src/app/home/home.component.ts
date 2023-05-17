23875145e8d8454f11b1b60213de5c2e6eb55257
AnujaNikumbh5 <anuja@bbd.co.za>
2023-05-17 18:46:14 +0530

    search field works!

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -72,39 +72,6 @@ export class HomeComponent implements OnInit {
   }
 
 
-//   usersList: any[] = [
-//     { Id: 1, Name: 'John Smith', property_name: '@example.com', user: '234', created_date: 'com', status: 'active', },
-//     { Id: 2, Name: 'anu', property_name: 'john.smith@example.com', user: '4', created_date: 'example.com', status: 'active', },
-//     { Id: 3, Name: 'Smith2', property_name: 'abc.smith@example.com', user: '555', created_date: 'www.example.com', status: 'active', },
-//   ];
-
-//   headArray = [
-//     { 'Head': 'Name', 'FieldName': 'Name' },
-//     { 'Head': 'property_name', 'FieldName': 'property_name' },
-//     { 'Head': 'user', 'FieldName': 'user' },
-//     { 'Head': 'created_date', 'FieldName': 'created_date' },
-//     { 'Head': 'status', 'FieldName': 'status' },
-//     { 'Head': 'Action', 'FieldName': '' }
-//   ];
-
-//   ngOnInit(): void {
-//     console.log('Users list:', this.usersList);
-//   }
-
-//   filterData() {
-//     console.log('Search value:', this.searchValue);
-//     if (!this.searchValue) {
-//       return this.usersList;
-//     }
-//     const filteredList = this.usersList.filter(user => {
-//       return user.Name.toLowerCase().includes(this.searchValue.toLowerCase());
-//     });
-//     console.log('Filtered list:', filteredList);
-//     return filteredList;
-//   }
-  
-
-
   search() {
     console.log('Search value:', this.searchValue);
     this.filterData();
