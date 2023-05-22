d2494180de21e4528f0751424258ae1fcfd0c46f
AnujaNikumbh5 <anuja@bbd.co.za>
2023-05-22 17:15:13 +0530

    latest

---
+++ src/app/dashboard/dashboard.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+  private apiUrl = 'http://localhost:8089/api';
+
+  constructor(private http: HttpClient) { }
+
+  getPropertyCount() {
+    return this.http.get<number>(`${this.apiUrl}/properties/count`);
+  }
+
+  getUserCount() {
+    return this.http.get<number>(`${this.apiUrl}/users/count`);
+  }
+  
+  getDocumentCount() {
+    return this.http.get<number>(`${this.apiUrl}/documents/count`);
+  }
+}
---
+++ src/app/property-owner-dashboard/property-owner-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-owner-dashboard',
+  templateUrl: './property-owner-dashboard.component.html',
+  styleUrls: ['./property-owner-dashboard.component.scss']
+})
+export class PropertyOwnerDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
