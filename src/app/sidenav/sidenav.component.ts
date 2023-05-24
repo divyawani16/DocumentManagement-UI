7249709c9c3255d7de7f11f2c874634f340ae351
AnujaNikumbh5 <anuja@bbd.co.za>
2023-05-24 19:14:04 +0530

    new dashboards

--- src/app/sidenav/sidenav.component.ts
+++ src/app/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router,NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.scss']
 })
 export class SidenavComponent implements OnInit {
+  @Input() isTenantDashboard: boolean;
+  @Input() isOwnerDashboard: boolean;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  isDefaultNavigation(): boolean {
+    return !this.isTenantDashboard && !this.isOwnerDashboard;
+  }
+
+  isTenantDashboardNavigation(): boolean {
+    return this.isTenantDashboard && !this.isOwnerDashboard;
+  }
+
+  isOwnerDashboardNavigation(): boolean {
+    return this.isOwnerDashboard;
+  }
 }
