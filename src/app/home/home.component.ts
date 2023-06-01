7281cecc2d9b18ce73f4a7b31233a92b4f7f70a0
divyawani16 <divyaw@bbd.co.za>
2023-06-01 11:07:34 +0530

    download logic added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { HomeService } from './home.service';
 import { Document } from './home.model';
 import { AddpropertyComponent } from '../addproperty/addproperty.component';
 import { HttpEventType } from '@angular/common/http';
+import { HttpResponse } from '@angular/common/http';
 
 
 @Component({
@@ -15,7 +16,6 @@ import { HttpEventType } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
   searchValue: string = '';
-
   documentsList: Document[] | undefined;
 
   constructor(
@@ -24,12 +24,12 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    try{
+    try {
       this.homeService.getDocuments()
-      .subscribe(documents => this.documentsList = documents);
-      console.log(this.documentsList)
-    }catch(err){
-      console.log(err)
+        .subscribe(documents => this.documentsList = documents);
+      console.log(this.documentsList);
+    } catch (err) {
+      console.log(err);
     }
   }
 
@@ -38,7 +38,6 @@ export class HomeComponent implements OnInit {
       width: '450px',
       height: '500px',
       data: {},
-
     });
 
     dialogRef.afterClosed().subscribe(result => {
@@ -47,14 +46,13 @@ export class HomeComponent implements OnInit {
     });
   }
 
-
-  headArray = [  
-    { 'Head': 'Document Name', 'FieldName': 'documentName' },  
-    { 'Head': 'User Name ', 'FieldName': 'userName' }, 
-    { 'Head': 'Property Name', 'FieldName': 'propertyName' },  
-    { 'Head': 'Document Type', 'FieldName': 'docTypeName' }, 
+  headArray = [
+    { 'Head': 'Document Name', 'FieldName': 'documentName' },
+    { 'Head': 'User Name ', 'FieldName': 'userName' },
+    { 'Head': 'Property Name', 'FieldName': 'propertyName' },
+    { 'Head': 'Document Type', 'FieldName': 'docTypeName' },
     { 'Head': 'Document Mime Type', 'FieldName': 'docMimeTypeName' },
-    {'Head': 'Action', 'FieldName': 'action' } ,
+    { 'Head': 'Action', 'FieldName': 'action' },
     { 'Head': 'View', 'FieldName': 'download' },
   ];
 
@@ -70,21 +68,21 @@ export class HomeComponent implements OnInit {
     return filteredList;
   }
 
-
   search() {
     console.log('Search value:', this.searchValue);
     this.filterData();
   }
-  editDocument(document: Document) {
 
+  editDocument(document: Document) {
+  
   }
- 
+
   deleteDocument(item: any) {
     const index = this.documentsList.indexOf(item);
     if (index > -1) {
       this.documentsList.splice(index, 1);
     }
-  
+
     this.homeService.deleteDocument(item.documentId).subscribe(
       () => {
         console.log('Record deleted successfully from the database');
@@ -94,26 +92,188 @@ export class HomeComponent implements OnInit {
       }
     );
   }
-  
 
-
-
-  download(item: any): void {
-  
+  public download(documentId: number){
+    console.log(documentId);
+    const documentt = this.documentsList.find(doc => doc.documentId === documentId);
+    
+    if (documentId) {
+      this.homeService.download (documentId).subscribe(
+        (response: HttpResponse<Blob>) => {
+          const contentDispositionHeader = response.headers.get('content-disposition');
+          const fileName = contentDispositionHeader?.split(';')[1].split('=')[1];
+          const url = window.URL.createObjectURL(response.body);
+          const a = document.createElement('a');
+          a.href = url;
+          a.download = fileName || 'document';
+          a.click();
+          window.URL.revokeObjectURL(url);
+        },
+        (error: any) => {
+          console.error('Error downloading document:', error);
+        }
+      );
+    } else {
+      console.error('Invalid documentId');
+    }
   }
+  
+  
+  
 
   print(item: any): void {
-  
+    // Handle printing
   }
-
-
 
   isClicked = false;
-  onSearchBoxClick() {
 
+  onSearchBoxClick() {
     this.isClicked = true;
   }
+}
+
+// import { Component, OnInit } from '@angular/core';
+// import { MatDialog } from '@angular/material/dialog';
+// import { AddDocumentComponent } from '../add-document/add-document.component';
+// import { MatIconModule } from '@angular/material/icon';
+// import { HomeService } from './home.service';
+// import { Document } from './home.model';
+// import { AddpropertyComponent } from '../addproperty/addproperty.component';
+// import { HttpEventType } from '@angular/common/http';
+// import { HttpResponse } from '@angular/common/http';
+
+
+// @Component({
+//   selector: 'app-home',
+//   templateUrl: './home.component.html',
+//   styleUrls: ['./home.component.scss']
+// })
+// export class HomeComponent implements OnInit {
+//   searchValue: string = '';
+
+//   documentsList: Document[] | undefined;
+
+//   constructor(
+//     private dialog: MatDialog,
+//     private homeService: HomeService
+//   ) {}
+
+//   ngOnInit() {
+//     try{
+//       this.homeService.getDocuments()
+//       .subscribe(documents => this.documentsList = documents);
+//       console.log(this.documentsList)
+//     }catch(err){
+//       console.log(err)
+//     }
+//   }
+
+//   openDialog(): void {
+//     const dialogRef = this.dialog.open(AddDocumentComponent, {
+//       width: '450px',
+//       height: '500px',
+//       data: {},
+
+//     });
+
+//     dialogRef.afterClosed().subscribe(result => {
+//       console.log('The dialog was closed');
+//       console.log(result);
+//     });
+//   }
+
+
+//   headArray = [  
+//     { 'Head': 'Document Name', 'FieldName': 'documentName' },  
+//     { 'Head': 'User Name ', 'FieldName': 'userName' }, 
+//     { 'Head': 'Property Name', 'FieldName': 'propertyName' },  
+//     { 'Head': 'Document Type', 'FieldName': 'docTypeName' }, 
+//     { 'Head': 'Document Mime Type', 'FieldName': 'docMimeTypeName' },
+//     {'Head': 'Action', 'FieldName': 'action' } ,
+//     { 'Head': 'View', 'FieldName': 'download' },
+//   ];
+
+//   filterData() {
+//     console.log('Search value:', this.searchValue);
+//     if (!this.searchValue) {
+//       return this.documentsList;
+//     }
+//     const filteredList = this.documentsList.filter(document => {
+//       return document.documentName.toLowerCase().includes(this.searchValue.toLowerCase());
+//     });
+//     console.log('Filtered list:', filteredList);
+//     return filteredList;
+//   }
+
+
+//   search() {
+//     console.log('Search value:', this.searchValue);
+//     this.filterData();
+//   }
+//   editDocument(document: Document) {
+
+//   }
+ 
+//   deleteDocument(item: any) {
+//     const index = this.documentsList.indexOf(item);
+//     if (index > -1) {
+//       this.documentsList.splice(index, 1);
+//     }
+  
+//     this.homeService.deleteDocument(item.documentId).subscribe(
+//       () => {
+//         console.log('Record deleted successfully from the database');
+//       },
+//       (error) => {
+//         console.error('Error deleting record from the database:', error);
+//       }
+//     );
+//   }
   
 
-}
+// // public download():void {
+// //   this.homeService.download()
+// //   .subscribe(response=>
+// //     {
+// //       let fileName= response.headers.get('content-disposition')
+// //       ?.split(';')[1].split('=')[1];
+// //       let blob: Blob= response.boby as Blob;
+// //       let a =document.createElement('a');
+// //       a.download=fileName;
+// //       a.href=window.URL.createObjectURL(blob);
+// //       a.click();
+// //     })
+// public download(documentId: number): void {
+//   console.log('Download method called with documentId:', documentId);
+//   this.homeService.downloadDocument(documentId)
+//     .subscribe((response: HttpResponse<Blob>) => {
+//       const contentDispositionHeader = response.headers.get('content-disposition');
+//       const fileName = contentDispositionHeader?.split(';')[1].split('=')[1];
+//       const url = window.URL.createObjectURL(response.body);
+//       const a = document.createElement('a');
+//       a.href = url;
+//       a.download = fileName;
+//       a.click();
+//       window.URL.revokeObjectURL(url);
+//     });
+// }
+
+
+
+
+
+//   print(item: any): void {
+  
+//   }
+
+
+
+//   isClicked = false;
+//   onSearchBoxClick() {
+
+//     this.isClicked = true;
+//   }
+  
+
+// }
 
--- src/app/home/home.service.ts
+++ src/app/home/home.service.ts
@@ -1,26 +1,33 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpProgressEvent, HttpEvent} from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { Document } from './home.model';
-@Injectable({
-  providedIn: 'root'
-})
-export class HomeService {
+  import { Injectable } from '@angular/core';
+  import { HttpClient, HttpProgressEvent, HttpEvent, HttpResponse} from '@angular/common/http';
+  import { Observable } from 'rxjs';
+  import { Document } from './home.model';
+  @Injectable({
+    providedIn: 'root'
+  })
+  export class HomeService {
 
-  private baseUrl:string;
+    private baseUrl:string;
+    httpClient: any;
 
-  constructor(private http: HttpClient) { 
-    this.baseUrl = 'http://localhost:8089/api/documents';
+    constructor(private http: HttpClient) { 
+      this.baseUrl = 'http://localhost:8089/api/documents';
+    }
+    
+    public getDocuments(): Observable<Document[]> {
+      return this.http.get<Document[]>(`${this.baseUrl}/documentsdetails`);
+    }
+    public createDocument(formData: FormData): Observable<any> {
+      return this.http.post<any>(this.baseUrl, formData);
+    }
+    public deleteDocument(documentId: number): Observable<void> {
+      const url = `${this.baseUrl}/${documentId}`;
+      return this.http.delete<void>(url);
+    }
+
+    public download(documentId: number): Observable<HttpResponse<Blob>> {
+      const url = `${this.baseUrl}/${documentId}/download`;
+      console.log(documentId);
+      return this.http.get(url, { observe: 'response', responseType: 'blob' });
+    }
   }
-  
-   public getDocuments(): Observable<Document[]> {
-    return this.http.get<Document[]>(`${this.baseUrl}/documentsdetails`);
-  }
-  public createDocument(formData: FormData): Observable<any> {
-    return this.http.post<any>(this.baseUrl, formData);
-  }
-  public deleteDocument(documentId: number): Observable<void> {
-    const url = `${this.baseUrl}/${documentId}`;
-    return this.http.delete<void>(url);
-  }
-}
